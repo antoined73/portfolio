c3855018937b3459e7852314d2fa8fcc20de202c
Antoine Dezarnaud <dezartoine@hotmail.fr>
2019-04-02 20:08:02 +0200

    Basic structure of web site + store & router

---
+++ src/components/views/ProjectView.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+        <p class="title" v-if="project">
+            {{project.title}}
+        </p>
+        <p class="subtitle" v-if="project">
+            {{project.description}}
+        </p>
+        </div>
+    </section>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectView',
+  components: {
+  },
+  data(){
+      return {
+          project : null
+      }
+  },
+  created(){
+      const projectId = parseInt(this.$route.params.projectId);
+      this.project = this.$store.getters['projects/getProjectById'](projectId);
+  }
+}
+</script>
